2be205544b9a997ff00423d3f1ed004e1fa59e17
Fawwaz <fawwaz.chowdhury@googlemail.com>
2024-12-09 16:53:47 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
 import Feed from "./components/Feed";
+import Stories from "./components/Stories";
 
 export default function Home() {
-  return <Feed />;
+  return (
+    <div className="home-page flex flex-col space-y-6 p-4">
+      {/* Stories Component */}
+      <Stories />
+
+      {/* Feed Component */}
+      <Feed />
+    </div>
+  );
 }
